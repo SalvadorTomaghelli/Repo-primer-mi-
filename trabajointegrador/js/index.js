0fafa9ff35280c914dcacae472de22205920c6b6
SalvadorTomaghelli <stomaghelli@udesa.edu.ar>
2023-11-04 11:31:48 -0300

    volvimos

---
+++ trabajointegrador/js/index.js
@@ -0,0 +1,11 @@
+let apiKey= '26cb00ba0e4d52cae073a420c45e2d99'
+fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`)
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    console.log(data)
+})
+.catch(function(error){
+    console.log(error)
+})
