d6c1e2c746bd4393716c30c8742742cb26563a51
Mateo Ruete <rmateo@udesa.edu.ar>
2023-11-10 10:22:26 -0300

    cambios en home

--- trabajointegrador/js/home.js
+++ trabajointegrador/js/home.js
@@ -1,5 +1,5 @@
 let container = document.querySelector(".section1")
-container2 = document.querySelector("#series")
+ let container2 = document.querySelector("#series")
 let apiKey= '26cb00ba0e4d52cae073a420c45e2d99'
 fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`)
 .then(function(response){
