eb3cf9277f745c86781e57585c90322ca2d308f5
SalvadorTomaghelli <stomaghelli@udesa.edu.ar>
2023-11-10 09:26:51 -0300

    api

--- trabajointegrador/js/home.js
+++ trabajointegrador/js/home.js
@@ -5,16 +5,15 @@ fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`)
     return response.json()
 })
 .then(function(data){
-    console.log(data)
     for(let i = 0; i < 5; i++){
         container.innerHTML += `
-        <article class = "articles">
-            <h2>${data.results[i].title}</h2>
-            <h2>${data.results[i].status}</h2>
-        <article>
-        `  
-    }
-    console.log(data)
+        <article class="articles">
+                        <a href="./detailmovie.html">
+                            <img src="./PeliculasPoster/oppen.jpg" alt=${data.results[i].title} class="img">
+                            <h3 class="descripcion">${data.results[i].title}<br>${data.results[i].release_date}</h3>
+                        </a>
+        </article>
+        `}
 })
 .catch(function(error){
     console.log(error)
