c3ec4517bf32334cd9329d0da2e0cdcdfbafd50b
Mateo Ruete <rmateo@udesa.edu.ar>
2023-11-10 09:49:36 -0300

    git push

--- trabajointegrador/js/home.js
+++ trabajointegrador/js/home.js
@@ -1,4 +1,5 @@
 let container = document.querySelector(".section1")
+container2 = document.querySelector("#series")
 let apiKey= '26cb00ba0e4d52cae073a420c45e2d99'
 fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`)
 .then(function(response){
@@ -14,7 +15,25 @@ fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=${apiKey}`)
                         </a>
         </article>
         `}
+    console.log(data)
 })
 .catch(function(error){
     console.log(error)
+})
+
+
+
+fetch(`https://api.themoviedb.org/3/tv/popular?api_key=${apiKey}`)
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    for(let i = 0; i < 5; i++){
+        container2.innerHTML +=`
+        <article class="articles">
+                        <a href="./detailserie.html"><img src="./SeriesPoster/bettercallsaul.jpg" alt=${data.results[i].title} class="img">
+                            <h3 class="descripcion">${data.results[i].title}<br>${data.results[i].release_date}</h3></a>
+                    </article>
+        `
+    }
 })
